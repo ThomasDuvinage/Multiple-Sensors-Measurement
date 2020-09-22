692f62a3b21fd39139df0ff6ef49753246908aa4
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-09-22 14:26:29 +0200

    create macros

--- 00 - Code/src/main.h
+++ 00 - Code/src/main.h
@@ -10,13 +10,32 @@
 #include "Voltmeter.h"
 #include "Accelerometer.h"
 #include "Ampmeter.h"
-//#include "Tachymeter.h"
+#include "Tachymeter.h"
 #include "MPU6050.h"
 
 #define INTERVAL 10
 #define SD_CS 10
 
 #define MPU_INIT_ITERATION
+
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_INFO(x)                         \
+    Serial.print(ampMeter.getCurrent());      \
+    Serial.print(" | ");                      \
+    Serial.print(voltMeter.getMeanVoltage()); \
+    Serial.print(" | ");                      \
+    Serial.print((float)normAccel.XAxis);     \
+    Serial.print(" | ");                      \
+    Serial.print((float)normAccel.YAxis);     \
+    Serial.print(" | ");                      \
+    Serial.print((float)normAccel.ZAxis);     \
+    Serial.print(" | ");                      \
+    Serial.println(tachymeter.getSpeed());
+#else
+#define DEBUG_INFO(x) x
+#endif
 
 #define ASSERT(x)               \
     Serial.print("Assert in "); \

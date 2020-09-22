39a061a4b7745ea3dca0fea8fe1fee491d3c4ab2
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-09-22 14:25:51 +0200

    implement tachymeter

--- 00 - Code/lib/Tachymeter/Tachymeter.h
+++ 00 - Code/lib/Tachymeter/Tachymeter.h
@@ -5,21 +5,29 @@
 
 // Motor encoder output pulse per rotation (change as required)
 #define ENC_COUNT_REV 1
+#define WHEEL_RADIUS 0.068
 
-volatile long actualTime;
-float frequency;
+#define CHECK_REPETITONS_ITERATIONS 20
+
+extern long actualTime;
+extern double frequency;
 
 class Tachymeter
 {
 private:
     int _pin;
-    float _rpm;
+    float _rpm, _previousRpm;
 
-    int interval;
+    int repetition_counter;
+    float initial_rpm;
+
+    float getRPM();
+
+    bool detectDecelaration();
 
 public:
     Tachymeter(int pin);
-    float getRPM();
+    float getSpeed();
 };
 
 #endif
